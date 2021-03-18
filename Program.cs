74cd6d11558a04cdddc01540bfd8bef785212b0e
Aurimas <aurim@DESKTOP-JO69DC8>
2021-03-18 10:39:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zaidimas_kartuves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kartuves = new Dictionary<int, string>
+            {
+                {0, " ------|\n|\n|\n|\n|\n|\n|\n|\n________" },
+                {1, " ------|\n|      o\n|\n|\n|\n|\n|\n|\n________" },
+                {2, " ------|\n|      o\n|      |\n|\n|\n|\n|\n|\n________" },
+                {3, " ------|\n|      o\n|      |\n|      O\n|\n|\n|\n|\n________" },
+                {4, " ------|\n|      o\n|     \\|\n|      O\n|\n|\n|\n|\n________" },
+                {5, " ------|\n|      o\n|     \\|/\n|      O\n|\n|\n|\n|\n________" },
+                {6, " ------|\n|      o\n|     \\|/\n|      O\n|     /\n|\n|\n|\n________" },
+                {7, " ------|\n|      o\n|     \\|/\n|      O\n|     / \\\n|\n|\n|\n________" }
+            };
+            
+            string[] vardai = new string[] {"Liutauras", "Dominykas", "Augustas", "Klemensas", "Aloyzas", "Aleksandras", "Marijonas", "Vytautas", "Anzelmas", "Gediminas"};
+            string[] miestai = new string[] { "Radviliškis", "Anykščiai", "Mažeikiai", "Virbalis", "Kybartai", "Ignalina", "Kalvarija", "Marijampolė", "Tauragė", "Jurbarkas"};
+            string[] valstybes = new string[] { "Argentina", "Brazilija", "Meksika", "Jamaika", "Alžyras", "Albanija", "Tailandas", "Kazakstanas", "Armėnija", "Garikija" };
+            string[] sportas = new string[] { "vandensvydis", "biatlonas", "šaškės", "penkiakovė", "maratonas", "futbolas", "beisbolas", "kriketas", "akmensvydis", "šachmatai" };
+
+
+            Console.WriteLine(string.Join('\n', kartuves));
+            Console.WriteLine("------------Press any key to continue---------------");
+            Console.ReadKey();
+        }
+
+
+    }
+}
